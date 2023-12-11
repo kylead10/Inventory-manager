d39ca16dd522a2a93e130b34cadd612e1fba2542
kylead10 <adiokayode111@gmail.com>
2023-12-11 23:17:28 +0100

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Login from './components/Login';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    // Check if email and password meet your criteria for a valid combination
-    if (isValidCombination(email, password)) {
-      // Successful login, you can add your login logic here
-      console.log('Login successful');
-      setError('');
-    } else {
-      setError('Invalid email or password');
-    }
-  };
-
-  const isValidCombination = (enteredEmail, enteredPassword) => {
-    // Implement your validation logic here
-    // For example, you might check against a predefined email and password
-    const validEmail = 'example@email.com';
-    const validPassword = 'password123';
-
-    return enteredEmail === validEmail && enteredPassword === validPassword;
-  };
-
+const App = () => {
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
-          <input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type='submit'>Login</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </form>
-    </div>
+    <>
+      <Login />
+    </>
   );
 };
 
-export default Login;
+export default App;
