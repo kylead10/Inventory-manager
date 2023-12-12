89d9b727205224655488d2ab99cf2198b409fefb
kylead10 <adiokayode111@gmail.com>
2023-12-12 13:36:26 +0100

    wrote a frame for routing

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  // Implement your authentication logic here
+  const isAuthenticated = false; // Replace with your authentication logic
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to='/login' />
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
