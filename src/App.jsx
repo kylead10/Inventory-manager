0748f719decbfb69841e1fad54152ed01f6b39dd
kylead10 <adiokayode111@gmail.com>
2023-12-12 13:35:04 +0100

    added other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
+import InventoryListing from './components/InventoryListing';
+import CreateInventory from './components/CreateInventory';
+import EditInventory from './components/EditInventory';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <PrivateRoute exact path='/' component={InventoryListing} />
+        <PrivateRoute path='/create' component={CreateInventory} />
+        <PrivateRoute path='/edit/:id' component={EditInventory} />
+      </Switch>
+    </Router>
   );
-};
+}
 
 export default App;
