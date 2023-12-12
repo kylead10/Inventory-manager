e8a9319d042ec6828d7f98460c32cd30a505d660
kylead10 <adiokayode111@gmail.com>
2023-12-12 13:37:58 +0100

    added inventory listing

---
+++ src/components/InventoryListing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const InventoryListing = () => {
+  // Implement your inventory listing logic here
+
+  return (
+    <div>
+      <h2>Inventory Listing</h2>
+      {/* Display your inventory data here */}
+    </div>
+  );
+};
+
+export default InventoryListing;
