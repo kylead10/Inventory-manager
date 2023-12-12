3ea9fe641def3b56bec0edaaa01877832877e4c8
kylead10 <adiokayode111@gmail.com>
2023-12-12 13:38:39 +0100

    create inventory function

---
+++ src/components/CreateInventory.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CreateInventory = () => {
+  // Implement your create inventory logic here
+
+  return (
+    <div>
+      <h2>Create Inventory</h2>
+      {/* Create inventory form */}
+    </div>
+  );
+};
+
+export default CreateInventory;
