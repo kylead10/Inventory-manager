d492a94b4a1152367be3d4e93d1a24eda825d65e
kylead10 <adiokayode111@gmail.com>
2023-12-15 22:20:39 +0100

    contd

--- src/components/InventoryListing.jsx
+++ src/components/InventoryListing.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../AuthContext';
 import { useNavigate } from 'react-router-dom';
+import './InventoryListingStyle.css';
 
 const InventoryListing = () => {
   const { user, logout } = useAuth();
